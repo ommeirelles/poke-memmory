bebef1c676800971d033dbc6195b4686850ec0c1
Vitor Hugo Brasiliense <vitor.hbr@outlook.com>
2021-03-10 15:55:22 -0300

    feat: Generate and Randomize PokemonCardArray

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
 import './styles/index.scss'
+import {Piece, Pieces} from './entity/index';
 
-export const x = 3;
+const pokemonCardArray: Array<Piece> = [];
+const positionsTaken: Array<number> = [];
+const tempPokemonCardArray: Array<Piece> = [];
+
+function fillPositions(numberOfCards: number) {
+  for(let i = 0; i < numberOfCards; i++) {
+    positionsTaken.push(i);
+  }
+}
+
+function fillPokemonCardArray () {
+  Pieces.forEach(pokemon => {
+    for(let i = 0; i < 2; i++) {
+      tempPokemonCardArray.push(pokemon);
+    }
+  });
+}
+
+function insertRandomPieces(numberOfCards: number) {
+  for(let countLoop = 0; countLoop < numberOfCards; countLoop++) {
+    const randomNumber = Math.floor(Math.random() * (18 - countLoop));
+    const position = positionsTaken.splice(randomNumber)[0];
+    pokemonCardArray[position] = tempPokemonCardArray.pop() as Piece;
+  }
+}
+
+function main() {
+  const numberOfCards = Pieces.length * 2;
+  fillPokemonCardArray();
+  fillPositions(numberOfCards);
+  insertRandomPieces(numberOfCards);
+  console.log(pokemonCardArray);
+}
+
+main();
