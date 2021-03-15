21d3422830a3b8318e77b65ea4015f2f5a9258a2
Vitor Hugo Brasiliense <vitor.hbr@outlook.com>
2021-03-15 16:34:15 -0300

    feat: Added Timer and Game Move Counters

--- src/game/script.ts
+++ src/game/script.ts
@@ -5,9 +5,14 @@ interface piecePageElement {
     piece: Piece;
 }
 
+let numberOfMoves: number = 0;
+let timeCount: number = 90;
 let selectedPieces: piecePageElement[] = [];
 let pokemonCardArray: Array<Piece>;
 let htmlCardArray: NodeListOf<Element>;
+const gameMovesSpan = document.querySelector('.game-moves');
+const timerSpan = document.querySelector('.timer');
+let goalCounter = 0;
 
 export function startGame(startingPokemonCardArray: Array<Piece>, startingHtmlCardArray: NodeListOf<Element>) {
     pokemonCardArray = startingPokemonCardArray;
@@ -16,13 +21,30 @@ export function startGame(startingPokemonCardArray: Array<Piece>, startingHtmlCa
     gameLoop();
 }
 
+let timerCounter: any;
+
 function gameLoop() {
-     console.log
+   timerCounter = setInterval(() => {
+        timeCount--;
+        if(timeCount === 0 || pokemonCardArray.length === goalCounter) finishGame();
+        let minutes = Math.floor(timeCount/60);
+        let seconds = timeCount % 60;
+        timerSpan!.innerHTML = `${String(('0' + minutes).slice(-2))} : ${String(('0' + seconds).slice(-2))}`;
+    }, 1000);
+}
+
+function finishGame() {
+    clearInterval(timerCounter);
+    if(pokemonCardArray.length === goalCounter){
+        console.log("You Win!");
+    } else {
+        console.log("You Lose!");
+    }
+    
 }
 
 export function cardHasBeenClicked(id: number) {
-    console.log(id);
-    if(htmlCardArray[id].classList.contains("matched") || htmlCardArray[id].classList.contains("selected")){
+    if(htmlCardArray[id].classList.contains("matched") || htmlCardArray[id].classList.contains("selected") || timeCount === 0){
         return;
     }
     if(selectedPieces.length < 2){
@@ -30,27 +52,25 @@ export function cardHasBeenClicked(id: number) {
         htmlCardArray[id].classList.toggle("selected");
         htmlCardArray[id].children[1].classList.toggle("selected");
         if(selectedPieces.length === 2){
+            numberOfMoves++;
+            gameMovesSpan!.innerHTML = String(('0' + numberOfMoves).slice(-2));
             const isEqual = isTwoPiecesEqual(selectedPieces[0].piece, selectedPieces[1].piece);
             selectedPieces.forEach(selectedPiece => {
                 if(isEqual){
                     htmlCardArray[selectedPiece.id].classList.toggle("matched");
-                    htmlCardArray[selectedPiece.id].children[1].classList.toggle("matched");
                     selectedPieces = [];
-                    
+                    goalCounter++;
                 }
                 else {
                     htmlCardArray[selectedPiece.id].classList.toggle("missed");
                     setTimeout(() => {
                         htmlCardArray[selectedPiece.id].classList.toggle("selected");
-                        htmlCardArray[selectedPiece.id].children[1].classList.toggle("selected");
                         htmlCardArray[selectedPiece.id].classList.toggle("missed");
-                        htmlCardArray[selectedPiece.id].children[1].classList.toggle("missed");
                         selectedPieces = [];
                     }, 2000);
                 }
             });
         }
     }
-
 }
 
